cd4d8e903128213a05309bb7ec853daffd481a9a
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-01-23 15:31:55 +0100

    add header component

---
+++ client/src/components/Footer/Footer.test.ts
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+// import { Footer } from './Footer';
+
+
+describe('Test Footer', () => {
+  let container: HTMLDivElement;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    // render(<Footer /> , container);
+  });
+
+})
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className='text-[#ce89a1] font-title font-bold text-7xl text-center p-5'>THE MILK STORE</header>
+  )
+}
+
+export default Header
