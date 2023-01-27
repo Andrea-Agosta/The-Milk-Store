261e8ef500a6f9b246b2a9702afb236ce2f878c2
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-01-27 14:15:00 +0100

    add allert component

---
+++ client/src/components/Allert/Allert.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Allert = () => {
+  return (
+    <div className='mt-64 bg-[#f7f7f7] rounded-xl shadow w-80 p-10 text-center'>
+      <p className='text-2xl mb-2'>Order Submitted</p>
+      <img src="https://www.kindpng.com/picc/m/106-1064894_check-mark-animated-gif-transparent-hd-png-download.png" alt="Check Mark"></img>
+    </div>
+  )
+}
